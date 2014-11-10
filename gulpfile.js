3e816692448de9b85d8db521d603f8c8446fd83a
klambycom <christian@klamby.com>
2014-11-11 00:57:54 +0100

    Tests

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp'),
     browserify = require('gulp-browserify'),
-    concat = require('gulp-concat');
+    concat = require('gulp-concat'),
+    jest = require('gulp-jest');
 
 gulp.task('browserify', function () {
   gulp.src('src/main.js')
@@ -12,6 +13,19 @@ gulp.task('browserify', function () {
 gulp.task('copyindex', function () {
   gulp.src('src/index.html')
     .pipe(gulp.dest('dist'));
+});
+
+gulp.task('test', function () {
+  return gulp.src('__tests__')
+    .pipe(jest({
+      testDirectoryName: 'spec',
+      scriptPreprocessor: './support/preprocessor.js',
+      unmockedModulePathPatterns: ['node_modules/react'],
+      testPathIgnorePatterns: [
+        "node_modules",
+        "./support"
+      ]
+    }));
 });
 
 gulp.task('default', ['browserify', 'copyindex']);
--- src/components/counter.js
+++ src/components/counter.js
@@ -1,3 +1,5 @@
+/** @jsx React.DOM */
+
 var React = require('react');
 var Firebase = require('firebase');
 
