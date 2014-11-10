ac399eec240e0cfc46c1e19d049429d9f0f9cddd
klambycom <christian@klamby.com>
2014-11-10 15:32:48 +0100

    First React-component

---
+++ src/components/app.js
@@ -0,0 +1,7 @@
+var App = React.createClass({
+  render: function () {
+    return (<h1>Hello, world!</h1>);
+  }
+});
+
+module.exports = App;
---
+++ src/main.js
@@ -0,0 +1,6 @@
+/** @jsx React.DOM */
+
+var App = require('./components/app'),
+    React = require('react');
+
+React.render(<App />, document.getElementById('main'));
