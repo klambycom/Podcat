15723689c81ee077e82634f4030ccb7227388d0d
klambycom <christian@klamby.com>
2014-12-05 12:05:20 +0100

    Get feed from YQL

--- src/components/feed.js
+++ src/components/feed.js
@@ -4,27 +4,68 @@ var React = require('react');
 var Episode = require('./episode');
 
 var Feed = React.createClass({
+  getInitialState: function () {
+    return { items: [] };
+  },
+  componentDidMount: function () {
+    var self = this;
+    var httpRequest = new XMLHttpRequest();
+    var subscriptions = JSON.parse(localStorage.getItem('podcat.subscriptions'));
+    var urls = Object
+      .keys(subscriptions)
+      .map(function (x) { return subscriptions[x].url; });
+
+    httpRequest.onreadystatechange = function () {
+      if (httpRequest.readyState === 4) {
+        if (httpRequest.status === 200) {
+          var result = JSON.parse(httpRequest.responseText);
+          var items = result.query.results.item.map(function (x) {
+            return {
+              title: x.title,
+              subtitle: x.subtitle,
+              image: x.image.href,
+              file: { url: x.enclosure.url }
+            };
+          });
+          self.setState({ items: items });
+
+          // array with objects, that have title, subtitle, image.href,
+          // enclosure.url
+        }
+      }
+    };
+
+    httpRequest.open(
+        'GET',
+        "http://query.yahooapis.com/v1/public/yql?q=select * from rss where url in ('" + urls.join("', '") + "') | sort(field=\"pubdate\", descending=\"true\")&format=json",
+        true);
+    httpRequest.send(null);
+  },
   render: function () {
+    // No episodes
+    var episodes = (
+        <div>
+          You have no episodes in your feed. Have you subscribed to any podcasts?
+        </div>
+        );
+
+    // List episodes
+    if (this.state.items.length > 0) {
+      episodes = this.state.items.map(function (item, i) {
+        return (
+            <Episode
+              key={i}
+              title={item.title}
+              image={item.image}
+              audio_url={item.file.url} />
+            );
+      });
+    }
+
     return (
         <div id="feed">
           <h1>Feed</h1>
-          <Episode
-            key='1'
-            title="Avsnitt 12 - Motionen om konungens återkomst"
-            image="http://assets.podomatic.net/ts/14/05/32/kmbengtsson88/1400x1400_9885800.jpeg"
-            audio_url="http://interasistmen.podomatic.com/enclosure/2014-11-17T15_52_52-08_00.mp3" />
-
-          <Episode
-            key='2'
-            title="Kodsnack 77 - Ett helt annat universum av saker"
-            image="http://static.libsyn.com/p/assets/2/b/3/1/2b31e735b8ae2a54/kodsnack3.png"
-            audio_url="http://traffic.libsyn.com/kodsnack/Uppkopplade_prylar.mp3" />
-
-          <Episode
-            key='3'
-            title="Kodsnack 76 - Fast på en viss punkt i verkligheten"
-            image="http://static.libsyn.com/p/assets/2/b/3/1/2b31e735b8ae2a54/kodsnack3.png"
-            audio_url="http://traffic.libsyn.com/kodsnack/singulariteten_frsk_2.mp3" />
+          {episodes}
         </div>
         );
   }
