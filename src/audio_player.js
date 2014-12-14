5c786723741729530311d54693bfba510f8c01af
klambycom <christian@klamby.com>
2014-12-14 23:56:31 +0100

    Ugly test hack

--- src/audio_player.js
+++ src/audio_player.js
@@ -1,1 +1,11 @@
-module.exports = new Audio();
+// Stupid tests fails without this because i don't understund how automock
+// works, I tink.
+var audio = {
+  addEventListener: function () {}
+};
+
+if (typeof window.Audio !== 'undefined') {
+  audio = new Audio();
+}
+
+module.exports = audio;
